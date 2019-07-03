fffbe5fde024c2bf30b5fbc8dbba88127ecc816c
akashpanjwani <33182006+akashpanjwani@users.noreply.github.com>
2019-07-03 09:02:24 +0530

    Add files via upload

--- TestDKBSapis/TestDKBSapis/Program.cs
+++ TestDKBSapis/TestDKBSapis/Program.cs
@@ -14,13 +14,14 @@ namespace TestDKBSapis
     {
         static void Main(string[] args)
         {
+
             string siteUrl = "https://bookon.dkbs.dk/";
 
             ClientContext clientContext = new ClientContext(siteUrl);
             clientContext.AuthenticationMode = ClientAuthenticationMode.FormsAuthentication;
             clientContext.FormsAuthenticationLoginInfo = new FormsAuthenticationLoginInfo("CRM Automation", "9LEkTny4");
             clientContext.ExecuteQuery();
-            string contentTypeName = "Bookinger";
+            // string contentTypeName = "Bookinger";
             Console.WriteLine(" Successfully Connected");
             List oList = clientContext.Web.Lists.GetByTitle("Bookinger");
             ListItemCollectionPosition itemPosition = null;
@@ -29,49 +30,45 @@ namespace TestDKBSapis
             clientContext.ExecuteQuery();
             foreach (ContentType ct in ctColl)
             {
-                if (ct.Name == contentTypeName && contentTypeName == "Bookinger")
+                if (ct.Name == "Bookinger" || ct.Name == "Gamle sager")
                 {
-                    //string query = $@"<View><ViewFields><FieldRef Name='Title'/></ViewFields>       
-                    //                      <Query>
-                    //                           <Where><Lt><FieldRef Name='ID' /><Value Type='Integer'>100</Value></Lt>
-                    //                            </Where>
-                    //                      <OrderBy><FieldRef Name='ID' /></OrderBy>
-                    //                       </Query><RowLimit>100</RowLimit></View>";
-
-                    string query = $@"<View>                            
-                                            <Query>
-                                                <Where>
-                                                    <And>
-                                                        <Geq>
-                                                            <FieldRef Name='ID' /><Value Type='Integer'>1000</Value>
-                                                        </Geq>
-                                                        <Leq>
-                                                            <FieldRef Name='ID' /><Value Type='Integer'>2000</Value>
-                                                        </Leq>                                        
-                                                    </And>
-                                                </Where>
-                                             </Query></View>";
-
-                    CamlQuery camlQuery = new CamlQuery();
-                    camlQuery.ListItemCollectionPosition = itemPosition;
-                    // camlQuery.ViewXml = @"<View><Query><RowLimit>500</RowLimit></Query></View>";
-                    // camlQuery.ViewXml = @"<View Scope='RecursiveAll'><RowLimit>5000</RowLimit></View>";
-                    camlQuery.ViewXml = query;
-                    ListItemCollection collListItem = oList.GetItems(camlQuery);
-
-                    clientContext.Load(collListItem);
-
-                    clientContext.ExecuteQuery();
-                    itemPosition = collListItem.ListItemCollectionPosition;
-                    Console.WriteLine(itemPosition);
-
-                    foreach (ListItem oListItem in collListItem)
+                    Console.WriteLine(ct.Name);
+                    Console.WriteLine("---Please wait while list item is displaying.---");
+                    try
                     {
-                        Console.WriteLine("ID: {0} \nTitle: {1} ", oListItem.Id, oListItem["Title"]);
+                        {
+                            string contentTypeName = "Bookinger";
+                            var query = new CamlQuery()
+                            {
+                                
+                                 ViewXml = String.Format("<View Scope='RecursiveAll'><Query><Where><Eq><FieldRef Name='ContentType' /><Value Type='Computed'>{0}</Value></Eq></Where></Query></View>", contentTypeName)
+                                // ViewXml = String.Format("<View Scope='RecursiveAll'><RowLimit>5000</RowLimit></View>")
+
+                            };
+
+                            ListItemCollection collListItem = oList.GetItems(query);
+
+                            clientContext.Load(collListItem);
+
+                            clientContext.ExecuteQuery();
+                            itemPosition = collListItem.ListItemCollectionPosition;
+                            Console.WriteLine(itemPosition);
+
+                            foreach (ListItem oListItem in collListItem)
+                            {
+                                Console.WriteLine("ID: {0} \nTitle: {1} ", oListItem.Id, oListItem["Title"]);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
                     }
                 }
             }
         }
 
-    }       
+    }
+
 }
+
