c50fec5615d1351688439f64d61697ba0aaabc13
akashpanjwani <33182006+akashpanjwani@users.noreply.github.com>
2019-07-05 20:53:33 +0530

    Add files via upload

--- TestDKBSapis/TestDKBSapis/Class1.cs
+++ TestDKBSapis/TestDKBSapis/Class1.cs
@@ -6,41 +6,55 @@ namespace Microsoft.SDK.SharePointServices.Samples
 {
     class RetrieveListItems
     {
-       public static void TestMethod1()
+        public static void TestMethod1()
         {
-            string siteUrl = "http://MyServer/sites/MySiteCollection";
+            string siteUrl = "https://bookon.dkbs.dk/";
 
             ClientContext clientContext = new ClientContext(siteUrl);
             clientContext.AuthenticationMode = ClientAuthenticationMode.FormsAuthentication;
             clientContext.FormsAuthenticationLoginInfo = new FormsAuthenticationLoginInfo("CRM Automation", "9LEkTny4");
             clientContext.ExecuteQuery();
-            SP.List oList = clientContext.Web.Lists.GetByTitle("Service katalog");
+            SP.List oList = clientContext.Web.Lists.GetByTitle("Partnere");
 
             CamlQuery camlQuery = new CamlQuery();
-            camlQuery.ViewXml = "<View><Query></Query></View>";
+            camlQuery.ViewXml = "<View Scope='RecursiveAll'><Query></Query></View>";
             ListItemCollection collListItem = oList.GetItems(camlQuery);
 
             clientContext.Load(collListItem);
 
             clientContext.ExecuteQuery();
 
-            // get partner listitem using this line, but you can use caml query to get many partner items
-
-            //ListItem partnerItem = lstCIs.GetItemById(lv.LookupId);
-
-            // get subsite url
-
-            //if (partnerItem["CISite"] != null)
-            //    {
-            //        FieldUrlValue subSiteUrl = partnerItem["CISite"] as FieldUrlValue;
-            //        using (ClientContext subsiteContext = new ClientContext(subSiteUrl.Url))
-            //        { }
-            //    }
-
             foreach (ListItem oListItem in collListItem)
             {
-                Console.WriteLine("ID: {0} \nTitle: {1} ", oListItem.Id, oListItem["Title"]);
+
+                var hyperLink = ((SP.FieldUrlValue)(oListItem["CISite"]));
+                if (hyperLink != null)
+                {
+                    Console.WriteLine("ID: {0} \nTitle: {1} \nSite: {2} \nSiteUrl: {3} ", oListItem.Id, oListItem["Title"], oListItem["CISite"], oListItem["CISiteShortUrl"]);
+                    var hLink = ((SP.FieldUrlValue)(oListItem["CISite"])).Url;
+                    Console.WriteLine(hLink);
+
+
+                    ClientContext Context = new ClientContext(hLink);
+                    Context.AuthenticationMode = ClientAuthenticationMode.FormsAuthentication;
+                    Context.FormsAuthenticationLoginInfo = new FormsAuthenticationLoginInfo("CRM Automation", "9LEkTny4");
+                    Context.ExecuteQuery();
+                    SP.List oListData = Context.Web.Lists.GetByTitle("Kursuspakke");
+
+                    camlQuery.ViewXml = "<View Scope='RecursiveAll'><Query></Query></View>";
+                    ListItemCollection oListDataItem = oListData.GetItems(camlQuery);
+
+                    Context.Load(oListDataItem);
+
+                    Context.ExecuteQuery();
+
+                    foreach (ListItem oItem in oListDataItem)
+                    {
+                        Console.WriteLine("ID: {0} \nTitle: {1}", oItem.Id, oItem["Title"]);
+                    }
+                }
             }
         }
+
     }
 }
